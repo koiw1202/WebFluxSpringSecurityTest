26c8bfd84de2e10855f06f1ff17db776149f8e4d
ksh <koiw1202@naver.com>
2024-02-20 21:17:23 +0900

    Flux 샘플 코드

--- src/main/java/com/example/SpringSecurityWebFluxApplication.java
+++ src/main/java/com/example/SpringSecurityWebFluxApplication.java
@@ -2,12 +2,26 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringSecurityWebFluxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityWebFluxApplication.class, args);
-    }
 
+        Flux<String> sequence = Flux.just("Hello", "Reactor") ;
+
+        sequence.map(String::toLowerCase)
+                .subscribe(System.out::println) ;
+
+//        SpringApplication.run(SpringSecurityWebFluxApplication.class, args);
+
+
+    }
 }
+
+
+
+
+
+
